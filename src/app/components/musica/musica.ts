49a7e2ad2ea9247b644a97471572b18136886186
Diego Fino <diego.fino@thomasgreg.com>
2026-01-22 01:15:24 -0500

    feat: integrate faker.js for dynamic music record generation and update styles

--- src/app/components/musica/musica.ts
+++ src/app/components/musica/musica.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-musica',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './musica.html',
   styleUrl: './musica.css',
 })
@@ -11,23 +13,95 @@ export class Musica {
   constructor() {
     this.records = [
       {
-        title: 'Song One',
-        artist: 'Artist A',
-        album: 'Album X',
-        year: 2020,
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2023 }),
         cover: 'https://picsum.photos/200/300?random=1'
       },      {
-        title: 'Song Two',
-        artist: 'Artist B',
-        album: 'Album Y',
-        year: 2021,
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
         cover: 'https://picsum.photos/200/300?random=2'
       },      {
-        title: 'Song Three',
-        artist: 'Artist C',
-        album: 'Album Z',
-        year: 2022,
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
         cover: 'https://picsum.photos/200/300?random=3'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=4'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=5'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=6'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=7'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=8'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=3'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=4'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=5'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=6'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=7'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=8'
+      },      {
+        title: faker.music.songName(),
+        artist: faker.music.artist(),
+        album: faker.music.album(),
+        year: faker.number.int({ min: 1990, max: 2024 }),
+        cover: 'https://picsum.photos/200/300?random=9'
       }];
   }
 }
